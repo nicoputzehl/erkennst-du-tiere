db1589ef870e94947d5147b7aff40883394df714
Nico Putzehl <nico.putzehl@gmail.com>
2025-07-13 13:40:58 +0200

    build in provider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -11,34 +11,53 @@ import "react-native-reanimated";
 import { useColorScheme } from "@/src/common/hooks/useColorScheme";
 import { ToastContainer } from "@/src/quiz/components/ToastContainer";
 import { QuizProvider } from "@/src/quiz/contexts/QuizProvider";
+import { Suspense } from 'react';
+import { ActivityIndicator } from 'react-native';
+import { SQLiteProvider, openDatabaseSync } from 'expo-sqlite';
+import { drizzle } from 'drizzle-orm/expo-sqlite';
+import { useMigrations } from 'drizzle-orm/expo-sqlite/migrator';
+import migrations from '@/drizzle/migrations';
+
+export const DATABASE_NAME = 'quizzes';
+
 
 export default function RootLayout() {
 	const colorScheme = useColorScheme();
 	const [loaded] = useFonts({ Ubuntu_400Regular });
+	const expoDb = openDatabaseSync(DATABASE_NAME);
+	const db = drizzle(expoDb);
+	const { success, error } = useMigrations(db, migrations);
 
 	if (!loaded) {
 		return null;
 	}
 
 	return (
-		<QuizProvider>
-			<ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-				<Stack screenOptions={{ headerShown: false }}>
-					<Stack.Screen name="index" options={{ headerShown: false }} />
-					<Stack.Screen
-						name="quiz/[quizId]/[questionId]/hints-modal"
-						options={{
-							presentation: "modal",
-							headerShown: false,
-							gestureEnabled: true,
-							animationDuration: 300,
-						}}
-					/>
-					<Stack.Screen name="+not-found" />
-				</Stack>
-				<StatusBar style="auto" />
-				<ToastContainer />
-			</ThemeProvider>
-		</QuizProvider>
+		<Suspense fallback={<ActivityIndicator size="large" />}>
+			<SQLiteProvider
+				databaseName={DATABASE_NAME}
+				options={{ enableChangeListener: true }}
+				useSuspense>
+				<QuizProvider>
+					<ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+						<Stack screenOptions={{ headerShown: false }}>
+							<Stack.Screen name="index" options={{ headerShown: false }} />
+							<Stack.Screen
+								name="quiz/[quizId]/[questionId]/hints-modal"
+								options={{
+									presentation: "modal",
+									headerShown: false,
+									gestureEnabled: true,
+									animationDuration: 300,
+								}}
+							/>
+							<Stack.Screen name="+not-found" />
+						</Stack>
+						<StatusBar style="auto" />
+						<ToastContainer />
+					</ThemeProvider>
+				</QuizProvider>
+			</SQLiteProvider>
+		</Suspense>
 	);
 }
